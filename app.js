d6fdeecbc0ce3e1a67b273373b54a36f01c74e0b
Zhiwu Mao <zwmao@outlook.com>
2022-09-24 16:02:14 +0800

    设置跨域

--- app.js
+++ app.js
@@ -27,6 +27,24 @@ const initExpress = () => {
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
 
+  app.all('*', (req, res, next) => {
+    // google需要配置，否则报错cors error
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    // 允许的地址,http://127.0.0.1:9000这样的格式
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    // 允许跨域请求的方法
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'POST, GET, OPTIONS, DELETE, PUT'
+    )
+    // 允许跨域请求header携带哪些东西
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since'
+    )
+    next()
+  })
+
   // #region 测试接口
   app.get('/', (req, res) => {
     res.send('Hello World!')
